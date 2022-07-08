d48c34b1d32b7ea1ba9d1564b078e90b3b47503b
Command8 <106775101+Command8@users.noreply.github.com>
2022-07-08 15:05:51 -0400

    Update get_fhir.py

--- get_fhir.py
+++ get_fhir.py
@@ -7,8 +7,8 @@ from h11 import Response
 
 payload = {"resourceType": "CapabilityStatement","name": "RestServer","status":"active"}
 
-#r = requests.get ('http://hapi.fhir.org/baseR4/metadata', params= payload)
-r = requests.get ('http://localhost:8080/fhir/metadata', params= payload)
+r = requests.get ('http://hapi.fhir.org/baseR4/metadata', params= payload)
+#r = requests.get ('http://localhost:8080/fhir/metadata', params= payload)
 r.status_code
 print (r.status_code)
 
